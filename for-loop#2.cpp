55ebe75513afcf0d98f574cf07cd0d38c04ab112
olganedelcu <olganedelcuam@gmail.com>
2021-02-17 17:07:29 +0100

    more on for loops

---
+++ for-loop#2.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+// Display a text 5 times
+
+int main() {
+    for (int i = 1, i <= 5; ++i) {
+        cout << "Hello World! " << endl;
+    }
+    return 0;
+}
---
+++ for_loops.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+// Printing numbers from 1 to 5
+
+int main(){
+    for (int i = 1; i <= 5; ++i) {
+        cout << i << " ";
+    }
+    return 0
+}

008166e462babcc90722f01f22f2b77297896224
olganedelcu <olganedelcuam@gmail.com>
2021-02-17 16:47:42 +0100

    git

---
+++ do-while-loop.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+
+int main(void){
+    int n = 50;
+    do{
+        cout << n << "\n";
+        n--;
+    }while (n>=1);
+}
--- iteration(while_loop)_example5.cpp
+++ iteration(while_loop)_example5.cpp
@@ -1,8 +1,11 @@
 #include<iostream>
 using namespace std;
-
-int main()
+// Print on the screen the numbers {50,49,...1} in increasing order
+int main(void)
 {
-    
-    return 0;
+    int n = 50;
+    while (n>=1){
+            cout << n << "\n";
+            n--;
+    }
 }

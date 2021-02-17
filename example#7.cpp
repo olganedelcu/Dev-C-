7ad71326edb6492654f9d541443949ecba24dc4f
olganedelcu <olganedelcuam@gmail.com>
2021-02-17 18:13:59 +0100

    more on exercises

--- example#7.cpp
+++ example#7.cpp
@@ -1,18 +1,20 @@
 #include<iostream>
+#include<cmath>
 using namespace std;
  
 int main()
 {
-    int val, num, sum = 0;
+    int val, num,a, sum = 0;
  
     cout << "Enter the number : ";
     cin >> val;
     num = val;
     while (num != 0)
     {
-        sum = sum + num % 10;
+        sum = sum + a % 10;
         num = num / 10;
+        a = pow(num,2);
     }
-    cout << "The sum of the digits of "
+    cout << "The sum of the square of the digits of "
          << val << " is " << sum;
 }

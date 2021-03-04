b0f5cd0d58bd3bc06504f4d88985915e3c5a55f3
olganedelcu <olganedelcuam@gmail.com>
2021-03-04 17:47:06 +0100

    more on while loops

--- homework#1.cpp
+++ homework#1.cpp
@@ -1,39 +1,59 @@
 #include <iostream>
 #include <cmath>
+#include<conio.h>
+#include<stdio.h>
 
 using namespace std;
 
-int main()
+int main(void)
 {
-    int num,val, sum;
+    int num,val1,val2,val3,its, sum;
     sum = 0;
     num = 0;
+    its = 1;
+    char a ;
+    a = getch();
+    bool happy = false;
 
     cout << "Introduce a number between 1 and 999(enter to exit): ";
-    cin >> val;
+    cin >> num;
 
-    if (val < 999 || val < 1)
-    // This loop will change val until it is one
-    	while (sum != 1) {
-        	/// The current digit we will calculate the square is the rightmost digit,
-        	//   so we just get its value using the mod operator 
-        	num = val;
-			int current = num % 10;
-        	// Add its square to the sum 
-        	sum += current*current;
-        	// You divide n by 10, this 'removes' one digit of n 
-        	num = num / 10;
-        	cout << "The sum of the squares = " << sum << endl;
-    	};
-    if (num == 1)
-    cout << val << " is a Happy Number!" << endl;
+    while ((num > 999 || num < 1)){
+        cout << "Number introduced is not in the range.Please enter another number: ";
+        cin >> num;
+    }
+    	
+    while (num != 1 &&  a != ' ' && its++){
+        if (num < 10){
+            val1 = num;
+            val2 = 0;
+            val3 = 0;
+        }else if ((num > 10) && (num < 999)){
+            val1 = num/10;
+            val2 = num % 10;
+            val3 = 0;
+        }else {
+            val1 = num/100;
+            val2= (num%100)/10;
+            val3=(num/100)%10;
+        }
+        sum = pow(val1,2) + pow(val2,2) + pow(val3,2);
+        num = sum;
+        num = a;
+        cout << "Iteration: (" <<its<<") the sum of the squares of the digits if the number is: " << sum;
+        
+    }
+
+    if (sum == 1){
+    	cout << num << " is a Happy Number!" << endl;
+	}
     
 
     // display sum
     cout << "\nThe sum is " << sum << endl;
 
-    if (num == '\n') // check to see if hit enter (\n)
+    if (a == '\n') // check to see if hit enter (\n)
         cout << "End by user decision" << endl;
-
+    
     return 0;
 }
